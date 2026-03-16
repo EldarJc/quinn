d4bf67bafc71cacad8bcdaa6d90c4a9c7a62248a
EldarJc <jusiceldar87@gmail.com>
2026-03-16 23:27:28 +0100

    Add CustomPage pagination class

--- backend/src/database/utils.py
+++ backend/src/database/utils.py
@@ -1,6 +1,23 @@
 from datetime import datetime, timezone
 
+from flask_smorest import Page
 from slugify import slugify
+
+from . import db
+
+
+class CustomPage(Page):
+    @property
+    def items(self):
+        query = self.collection.offset(self.page_params.first_item).limit(
+            self.page_params.page_size
+        )
+        return db.session.scalars(query).all()
+
+    @property
+    def item_count(self):
+        query = db.select(db.func.count()).select_from(self.collection.subquery())
+        return db.session.scalar(query)
 
 
 def utc_now() -> datetime:
