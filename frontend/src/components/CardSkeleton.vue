770441098d4ef6e8398c0453aa761c6461811f02
EldarJc <jusiceldar87@gmail.com>
2026-05-12 22:44:49 +0200

    Add CardSkeleton loader component

---
+++ frontend/src/components/CardSkeleton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-white rounded-2xl overflow-hidden shadow-sm border border-gray-200 animate-pulse h-full flex flex-col">
+    <div class="h-48 w-full bg-gray-200"></div>
+    <div class="p-6 flex flex-col grow space-y-4">
+      <div class="h-4 bg-gray-200 rounded w-1/3"></div>
+      <div class="h-6 bg-gray-200 rounded w-3/4"></div>
+      <div class="space-y-2 grow">
+        <div class="h-4 bg-gray-200 rounded w-full"></div>
+        <div class="h-4 bg-gray-200 rounded w-5/6"></div>
+      </div>
+      <div class="pt-4 border-t border-gray-100 flex justify-between">
+        <div class="h-6 bg-gray-200 rounded w-1/4"></div>
+        <div class="h-6 bg-gray-200 rounded w-16"></div>
+      </div>
+    </div>
+  </div>
+</template>
